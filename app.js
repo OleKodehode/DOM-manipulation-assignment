d2c414934b8a35476f4ab1f2b90ba23e64f528e1
Ole Kodehode <ole.Kodehode@gmail.com>
2025-08-06 21:25:37 +0200

    Changed around the structure and style a bit

--- app.js
+++ app.js
@@ -2,8 +2,6 @@ const scoreTxt = document.querySelector("#score-amount");
 const playBtn = document.querySelector("#play-btn");
 const playArea = document.querySelector("#playarea");
 const root = document.querySelector(":root"); // to get css variables
-let amount = 0;
-let spawning = false;
 const bubbleColors = [
   "hsla(280, 100%, 75%, 0.8)",
   "hsla(220, 100%, 75%, 0.8)",
@@ -17,14 +15,9 @@ playBtn.addEventListener("click", startGame);
 
 // root.style.setProperty("--bubble-color", bubbleColors[0]);  <- setting different color
 
-while (spawning == true) {
-  setInterval(spawnBubble(), 1000);
-}
-
 function startGame() {
   scoreTxt.textContent = "0"; // reset score
   playBtn.classList.toggle("hidden"); // hide button through class and styling
-  spawning = true;
 }
 
 function spawnBubble() {
